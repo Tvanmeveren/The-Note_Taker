6116fb06f783b1e3d7d68184a94d12e5f1d60ac4
Tvanmeveren <Tyvan07@yahoo.com>
2022-11-15 22:20:24 -0600

    added more const

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express =require('express');
+const path = require('path');
+const fs =require("fs")
 
 const { notes } = require('./db/db.json')
 const apiRoutes = require('./routes/apiRoutes')
